5bdf61e823968266cc1777242df67f99bd0e94e9
jetoile <kmx.petals@gmail.com>
2015-06-25 19:42:27 +0200

    change scenario

--- src/test/scala/fr/jetoile/sample/BasicScenario.scala
+++ src/test/scala/fr/jetoile/sample/BasicScenario.scala
@@ -11,7 +11,7 @@ object BasicScenario {
   var count = 0
   val random = new util.Random
 //  def generateMessage(): String = { "message" + random.nextString(20) }
-  def generateMessage(): String = { "message" + random.nextInt(20) }
+  def generateMessage(): String = { "message" + random.nextInt(100) }
 
 
   val feeder = Iterator.continually(Map("email" -> generateMessage() ))
@@ -21,10 +21,20 @@ object BasicScenario {
     .repeat(10,"count") {
     feed(feeder)
       .exec(http("req1").get("${email}"))
-      .pause(80 milliseconds, 1600 milliseconds)
-      .exec(
-        http("req2").get("${email}")
-      )
-      .pause(80 milliseconds, 1600 milliseconds)
+      .pause(100 milliseconds, 1600 milliseconds)
+      .exec(http("req2").get("${email}"))
+      .pause(120 milliseconds, 1600 milliseconds)
+      .exec(http("req3").get("${email}"))
+      .pause(120 milliseconds, 1600 milliseconds)
+      .exec(http("req4").get("${email}"))
+      .pause(120 milliseconds, 1600 milliseconds)
+      .exec(http("req5").get("${email}"))
+      .pause(120 milliseconds, 1600 milliseconds)
+      .exec(http("req6").get("${email}"))
+      .pause(120 milliseconds, 1600 milliseconds)
+      .exec(http("req7").get("${email}"))
+//      .pause(120 milliseconds, 1600 milliseconds)      
+//      .exec(http("req8").get("${email}"))
+//      .pause(120 milliseconds, 1600 milliseconds)            
   }
 }
--- src/test/scala/fr/jetoile/sample/BasicSimulation.scala
+++ src/test/scala/fr/jetoile/sample/BasicSimulation.scala
@@ -7,14 +7,15 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
 
   val httpConf = http
-    .baseURL("http://localhost:8081/sample/say/") // Here is the root for all relative URLs
+    .baseURL("http://192.168.0.103:8081/sample/say/") // Here is the root for all relative URLs
 
   var scn = BasicScenario.scn
 
 //  setUp(scn.inject(atOnceUsers(6000)).protocols(httpConf))
 
   setUp(scn.inject(
-  rampUsersPerSec(10) to 600 during(100 seconds),
-    atOnceUsers(6000)
+  rampUsersPerSec(10) to 100 during(20 seconds),
+//	rampUsers(6000) over(100 seconds),
+    atOnceUsers(100)
   ).protocols(httpConf))
 }
